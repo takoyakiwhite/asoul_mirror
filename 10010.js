50ef54efdedc4da39877ffe21714897ee372bf11
Takoyaki White <74053919+takoyakiwhite@users.noreply.github.com>
2026-03-30 10:53:02 +0800

    Update 10010.js

--- 10010.js
+++ 10010.js
@@ -1,7 +1,19 @@
 try {
     let body = $response.body;
-    if (!body || !body.includes("0211")) return $done({});
-    $done({ body: body.replace(/0211/g, "0000") });
-} catch {
+
+    if (!body) {
+        $done({});
+        return;
+    }
+
+    if (body.includes("0211")) {
+        body = body.replace(/0211/g, "0000");
+        console.log("替换成功");
+    }
+
+    $done({ body });
+
+} catch (e) {
+    console.log("错误: " + e);
     $done({});
 }
