9411a619646491f2193d6f714e2b00e52a20e372
Takoyaki White <74053919+takoyakiwhite@users.noreply.github.com>
2026-04-02 14:44:10 +0800

    Update 10010_surge.js

--- 10010_surge.js
+++ 10010_surge.js
@@ -1,22 +1,5 @@
-// Surge Response Script
-
-if (!$response.body) {
-  console.log("No body");
-  $done({});
-}
-
-let body = $response.body;
-
-try {
-  // 直接字符串替换（最稳）
-  let modified = body.replace(/0211/g, "0000");
-
-  console.log("Original:", body);
-  console.log("Modified:", modified);
-
-  $done({ body: modified });
-
-} catch (e) {
-  console.log("Error:", e);
-  $done({});
-}
+$done({
+  body: $response.body
+    ? $response.body.replace(/0211/g, "0000")
+    : $response.body
+});
